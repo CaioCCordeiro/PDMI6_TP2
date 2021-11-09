26310b6c9541b94a17252be79db1a35346bc6596
Caio Costa Cordeiro <caio.c.cordeiro@outlook.com>
2021-11-08 22:26:51 -0300

    Add project files.

---
+++ PDMI6_TP2/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDMI6_TP2
+{
+    class Produto
+    {
+        public string Id { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
---
+++ PDMI6_TP2/ViewSimplesStaticPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDMI6_TP2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewSimplesStaticPage : ContentPage
+    {
+        public ViewSimplesStaticPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
